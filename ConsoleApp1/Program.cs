b84b1d73276fbdc0349d60a8f39d0973c562c14f
Francisco Vargas G <52474105+f-vargasg@users.noreply.github.com>
2021-08-14 21:22:29 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+/*
+var lines = File.ReadAllLines("TextFile1.txt");
+foreach (var item in lines)
+{
+    Console.WriteLine(item);
+}
+*/
+
+//async Task ReadFile()
+//{
+//    var lines = await File.ReadAllLinesAsync("TextFle1.txt");
+//    foreach (var line in lines)
+//    {
+//        Console.WriteLine(line);
+//    }
+//}
+
+// await ReadFile();
+
+
+static async Task<int> GetDataFromNetworkAsync()
+{
+    // Simulate Network call
+    await Task.Delay(150);
+    var result = 42;
+
+    return result;
+
+}
+
+var networkResult = await GetDataFromNetworkAsync();
+
+// escribir la función en formato lamda
+Func<Task<int>> getDataFromNetworkViaLambda = async () =>
+{
+    // simula network call
+    await Task.Delay(150);
+   var result = 42;
+
+   return result;
+};
+
+
+//File.ReadAllLinesAsync("TextFile1.txt")
+//    .ContinueWith(t =>
+//    {
+//        if (t.IsFaulted)
+//        {
+//            Console.Error.WriteLine(t.Exception);
+//            return;
+//        }
+//        When the Task will be completed!
+//        foreach (var line in t.Result)
+//        {
+//            Console.WriteLine(line);
+//        }
+//    });
+//Console.WriteLine("I am here ");
+//Console.ReadKey();
+
+
+// haciendo sincronicamente 
+/*
+fileReadTask = File.ReadAllLinesAsync("TextFile1.txt");
+fileReadTask.Wait();
+*/
